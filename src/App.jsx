ca1dc7966393e5fb7789535f7ff0f2699d1e0d23
feelware <83146012+feelware@users.noreply.github.com>
2023-10-18 05:53:44 -0500

    ready for production

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Home from './Home'
 import Journey from './Journey'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?response_type=token&client_id=b9db9658c1ce47e1820adf95d965c6f0&scope=user-top-read&redirect_uri=http://localhost:5173/callback"
+const AUTH_URL = "https://accounts.spotify.com/authorize?response_type=token&client_id=b9db9658c1ce47e1820adf95d965c6f0&scope=user-top-read&redirect_uri=https://permutify.vercel.app/"
 
 const auth = () => {
     window.location.href = AUTH_URL;
